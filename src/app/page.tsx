c9de678620dc36b1178585ad7d9065cef7a75013
Maks <maxkov19861411@gmail.com>
2024-07-21 21:46:31 +0300

    Added new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import StatusLabel, { Status } from './components/StatusLabel/StatusLabel';
+// import StatusLabel, { Status } from './components/StatusLabel/StatusLabel';
+// import { headers } from 'next/headers';
+import AddCompanyButton from './components/AddCompanyButton';
 
 export default function Home() {
+  // console.log(headers());
   return (
     <main>
-      <h2 className="text-red-600 font-bold">Home page</h2>
+      {/* <h2 className="text-red-600 font-bold">
+        Home page{new Date().toTimeString()}
+      </h2>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
+      <AddCompanyButton />
     </main>
   );
 }
